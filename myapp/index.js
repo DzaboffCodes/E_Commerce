3c1685d132196cf5c7933fb2768985c64fc329a8
DzaboffCodes <142997851+DzaboffCodes@users.noreply.github.com>
2026-02-26 05:48:53 -0600

    Added a test route to determine if app was connected to db

--- myapp/index.js
+++ myapp/index.js
@@ -1,6 +1,9 @@
 // Import Express Library 
 const express = require('express');
 
+// Import database
+const db = require('./db');
+
 // Return an instance of an Express application
 const app = express();
 
@@ -11,6 +14,20 @@ const PORT = 3000;
 app.get('/',(req, res) => {
     res.send('Welcome to the E-Commerce API!');
 });
+
+// Test Get Route for db connection
+app.get('/db-test', async(req, res) => {
+    try {
+        const result = await db.query('SELECT NOW()');
+        res.json({
+            message: "Database connected",
+            time: result.rows[0]
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Database connection failed.")
+    }
+})
 
 // App listen
 app.listen(PORT, () => {
